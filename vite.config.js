265ef19214c9b32004c2baf339a3ed331b9a1751
Abhishek Sharma <abhello12@gmail.com>
2025-09-16 02:23:45 +0530

    fixing3

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['axios'], // Externalize axios
+      external: ['@reduxjs/toolkit'], // Externalize @reduxjs/toolkit
     },
   },
 });
